e419a59ca5f92df17d61addb6b372504e139f80c
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-04-30 15:14:22 -0300

    Add whatsapp

--- scripts/personal.ps1
+++ scripts/personal.ps1
@@ -3,4 +3,5 @@ winget install -e --id File-New-Project.EarTrumpet
 winget install -e --id Discord.Discord;
 winget install -e --id Nvidia.GeForceExperience;
 winget install -e --id Nvidia.GeForceNow;
-winget install -e --id Valve.Steam
+winget install -e --id Valve.Steam;
+winget install -e --id WhatsApp.WhatsApp
