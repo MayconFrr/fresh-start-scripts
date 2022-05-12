c5d00732924741951b6ae06b8ed9eee5fef261be
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-05-12 10:31:17 -0300

    Add Figma

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -6,5 +6,6 @@ winget install -e --id EclipseAdoptium.Temurin.17;
 winget install -e --id Microsoft.VisualStudioCode;
 winget install -e --id JetBrains.Toolbox;
 winget install -e --id Heroku.HerokuCLI;
+winget install -e --id Figma.Figma;
 wsl --update;
 wsl --install -d Ubuntu
