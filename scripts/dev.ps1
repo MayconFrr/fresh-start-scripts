57efa438eeacf64415e98f3cadef23fdcfd6a35d
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-05-13 15:59:08 -0300

    Remove minkube, doesn't work well with docker

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -1,6 +1,5 @@
 winget install -e --id Git.Git;
 winget install -e --id Docker.DockerDesktop;
-winget install -e --id Kubernetes.minikube;
 winget install -e --id OpenJS.NodeJS.LTS;
 winget install -e --id EclipseAdoptium.Temurin.17;
 winget install -e --id Microsoft.VisualStudioCode;
