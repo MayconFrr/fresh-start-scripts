1c9a388eabd73d4c9ec3540f700c456da0dade76
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-06-20 13:45:26 -0300

    Update personal.ps1

--- scripts/personal.ps1
+++ scripts/personal.ps1
@@ -2,5 +2,6 @@ winget install -e --id File-New-Project.EarTrumpet
 winget install -e --id Discord.Discord;
 winget install -e --id Nvidia.GeForceExperience;
 winget install -e --id Nvidia.GeForceNow;
+winget install -e --id OBSProject.OBSStudio
 winget install -e --id Valve.Steam;
-winget install -e --id WhatsApp.WhatsApp
+winget install -e --id WhatsApp.WhatsApp;
