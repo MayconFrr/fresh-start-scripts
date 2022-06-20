720c792a930be7c8112b728f14ff9ff0004753f6
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-06-20 13:39:32 -0300

    Update dev.psi1

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -1,10 +1,11 @@
-winget install -e --id Git.Git;
-winget install -e --id Docker.DockerDesktop;
-winget install -e --id OpenJS.NodeJS.LTS;
 winget install -e --id EclipseAdoptium.Temurin.17;
-winget install -e --id Microsoft.VisualStudioCode;
-winget install -e --id JetBrains.Toolbox;
-winget install -e --id Heroku.HerokuCLI;
 winget install -e --id Figma.Figma;
+winget install -e --id Heroku.HerokuCLI;
+winget install -e --id Git.Git;
+winget install -e --id JetBrains.Toolbox;
+winget install -e --id OpenJS.NodeJS.LTS;
+winget install -e --id Postman.Postman;
+wsl --install -d Ubuntu;
+
 wsl --update;
-wsl --install -d Ubuntu
+winget install -e --id Docker.DockerDesktop;
