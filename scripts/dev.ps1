f0cfd1aa174bd1473c5c6596fbbb032d60f1cf58
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-09-04 17:47:27 -0300

    Update dev.ps1

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -5,6 +5,7 @@ winget install -e --id EclipseAdoptium.Temurin.17;
 winget install -e --id Figma.Figma;
 winget install -e --id Git.Git;
 winget install -e --id JetBrains.Toolbox;
+winget install -e --id Microsoft.WindowsTerminal;
 winget install -e --id OpenJS.NodeJS.LTS;
 winget install -e --id Postman.Postman;
 winget install -e --id SlackTechnologies.Slack;
