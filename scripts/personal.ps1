e3218be7431feb66a8795151c62364f2496d3809
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-09-04 18:20:46 -0300

    Update personal.ps1

--- scripts/personal.ps1
+++ scripts/personal.ps1
@@ -1,5 +1,6 @@
 winget install -e --id File-New-Project.EarTrumpet
 winget install -e --id Discord.Discord;
+winget install -e --id flux.flux;
 winget install -e --id Nvidia.GeForceExperience;
 winget install -e --id Nvidia.GeForceNow;
 winget install -e --id OBSProject.OBSStudio;
